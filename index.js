aa871191c18e668558daa3029d1d8b3a49fafb1a
Winyumi <winguy2003@hotmail.com>
2019-12-08 20:21:21 -0500

    add route to index.html

--- index.js
+++ index.js
@@ -90,6 +90,9 @@ app.delete("/api/notes/:id", function(req, res) {
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 /*
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
